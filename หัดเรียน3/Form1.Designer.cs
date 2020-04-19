14b68046d17f95bf9fc625fd1222467e4be1de5d
panupongkongkate <61161511+panupongkongkate@users.noreply.github.com>
2020-04-19 13:07:02 +0700

    emgu count coin

--- หัดเรียน3/Form1.Designer.cs
+++ หัดเรียน3/Form1.Designer.cs
@@ -29,12 +29,221 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.imageBox2 = new Emgu.CV.UI.ImageBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.imageBox3 = new Emgu.CV.UI.ImageBox();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.imageBox4 = new Emgu.CV.UI.ImageBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+         //   this.histogramBox3 = new Emgu.CV.UI.HistogramBox();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageBox2
+            // 
+            this.imageBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.imageBox2.Location = new System.Drawing.Point(189, 28);
+            this.imageBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.imageBox2.Name = "imageBox2";
+            this.imageBox2.Size = new System.Drawing.Size(495, 340);
+            this.imageBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imageBox2.TabIndex = 3;
+            this.imageBox2.TabStop = false;
+            this.imageBox2.Click += new System.EventHandler(this.imageBox2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 28);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(155, 119);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Load";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // imageBox3
+            // 
+            this.imageBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.imageBox3.Location = new System.Drawing.Point(720, 27);
+            this.imageBox3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.imageBox3.Name = "imageBox3";
+            this.imageBox3.Size = new System.Drawing.Size(495, 340);
+            this.imageBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imageBox3.TabIndex = 7;
+            this.imageBox3.TabStop = false;
+            this.imageBox3.Click += new System.EventHandler(this.imageBox3_Click);
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.imageBox1.Location = new System.Drawing.Point(1251, 28);
+            this.imageBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(495, 340);
+            this.imageBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imageBox1.TabIndex = 9;
+            this.imageBox1.TabStop = false;
+            this.imageBox1.Click += new System.EventHandler(this.imageBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label1.Location = new System.Drawing.Point(257, 374);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(321, 29);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "SmoothGaussian+Equalize";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label2.Location = new System.Drawing.Point(863, 369);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(172, 29);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "otsu threshold";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label3.Location = new System.Drawing.Point(1347, 385);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(334, 29);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "canny edge detection+Dilate";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // imageBox4
+            // 
+            this.imageBox4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.imageBox4.Location = new System.Drawing.Point(720, 404);
+            this.imageBox4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.imageBox4.Name = "imageBox4";
+            this.imageBox4.Size = new System.Drawing.Size(495, 340);
+            this.imageBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imageBox4.TabIndex = 14;
+            this.imageBox4.TabStop = false;
+            this.imageBox4.Click += new System.EventHandler(this.imageBox4_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label4.Location = new System.Drawing.Point(865, 768);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(162, 39);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "ภาพเหรียญ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.textBox1.Location = new System.Drawing.Point(1352, 460);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(335, 228);
+            this.textBox1.TabIndex = 16;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label5.Location = new System.Drawing.Point(1412, 706);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(194, 39);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "จำนวนเหรียญ";
+            // 
+            // histogramBox3
+            // 
+            /*
+            this.histogramBox3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.histogramBox3.Location = new System.Drawing.Point(189, 431);
+            this.histogramBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.histogramBox3.Name = "histogramBox3";
+            this.histogramBox3.Size = new System.Drawing.Size(495, 323);
+            this.histogramBox3.TabIndex = 18;
+            this.histogramBox3.Load += new System.EventHandler(this.histogramBox3_Load_1);
+            */
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label6.Location = new System.Drawing.Point(336, 778);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(168, 29);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "histogramBox";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1848, 831);
+            this.Controls.Add(this.label6);
+//            this.Controls.Add(this.histogramBox3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.imageBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.imageBox1);
+            this.Controls.Add(this.imageBox3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.imageBox2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private Emgu.CV.UI.ImageBox imageBox2;
+        private System.Windows.Forms.Button button1;
+        private Emgu.CV.UI.HistogramBox histogramBox1;
+        private Emgu.CV.UI.HistogramBox histogramBox2;
+        private Emgu.CV.UI.ImageBox imageBox3;
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private Emgu.CV.UI.ImageBox imageBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label5;
+        private Emgu.CV.UI.HistogramBox histogramBox3;
+        private System.Windows.Forms.Label label6;
     }
 }
 
--- หัดเรียน3/Form1.cs
+++ หัดเรียน3/Form1.cs
@@ -7,14 +7,162 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.Util;
+using Emgu.CV.Structure;
+
 
 namespace หัดเรียน3
 {
     public partial class Form1 : Form
     {
+        OpenFileDialog op = new OpenFileDialog();
+        Image<Bgr, byte> image1;
+        Image<Gray, byte> thould;
+        Image<Gray, byte> Binarization; //แปลงภาพไบนารี่
+        Image<Gray, byte> cny;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (op.ShowDialog() == DialogResult.OK)
+            {
+                //แปลงภาพเป็น สีเทา
+
+                image1 = new Image<Bgr, byte>(op.FileName);
+                image1 = image1.Resize(1200, 800, Emgu.CV.CvEnum.Inter.Linear, true);
+  
+                Image<Gray, byte> Gray = image1.Convert<Gray, byte>();
+                //----------------------------------------------
+                //เบลอภาพด้วย SmoothGaussian
+                Gray = Gray.SmoothGaussian(9);
+                //ทำการEqualize
+                Gray._EqualizeHist();
+                imageBox2.Image = Gray;
+                //---------------------------------------------
+                
+
+                //------
+                // การทำBinarization เป็นภาพแบบ ไบนารี่ 
+                Binarization = new Image<Gray, byte>(Gray.Width, Gray.Height, new Gray(0));
+                //เป็นการทำ Otsu Threshold
+                CvInvoke.Threshold(Gray, Binarization, 0, 255, Emgu.CV.CvEnum.ThresholdType.Otsu | Emgu.CV.CvEnum.ThresholdType.Binary);
+                //ในlaw บอกว่าต้องใช่Otsu ร่วมกับ Binary ด้วย
+                imageBox3.Image = Binarization;
+                histogramBox3.ClearHistogram();
+                histogramBox3.GenerateHistograms(Gray, 256);
+                histogramBox3.Refresh();
+                //---------------------------------------------------------
+                // หาขอบภาพด้วย canny edge detection
+                cny = Binarization.Canny(180,120);
+               
+                //-----------------การขยายภาพDilate----------------
+                cny._Dilate(3);
+
+
+                // -----ทำการConvolution ภาพ
+                
+                ConvolutionKernelF convolutionKernelF = new ConvolutionKernelF(3,3);
+               
+                cny.Convolution(convolutionKernelF);
+                
+                imageBox1.Image = cny;
+                
+                //---------------------วาดภาพ ---------------------------------
+                
+
+                //Call HoughCircles (Canny included)
+
+                CircleF[] circleFs = cny.HoughCircles(
+                    new Gray(180), //cannyThreshold
+                    new Gray(450), //accumulatorThreshold
+                    6.0, //dp
+                    15.0, //minDist
+                    5, //minRadius
+                    0 //maxRadius
+                    )[0];
+                // Draw circles
+                int outputva = 0 ;//ตัวแปรนับเหรียญ
+                Image<Bgr, Byte> imageCircles = new Image<Bgr, byte>(cny.Size) ;
+                foreach (CircleF circle in circleFs)
+                {
+                    imageCircles.Draw(circle, new Bgr(Color.Red), 5);
+                    outputva++;
+                }
+                
+                //show 
+                imageBox4.Image = imageCircles;
+                textBox1.Text = outputva.ToString();
+                
+            }
+        }
+
+        private void histogramBox1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void histogramBox3_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void imageBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void imageBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void histogramBox2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void imageBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void imageBox4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void imageBox5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void histogramBox3_Load_1(object sender, EventArgs e)
+        {
+
         }
     }
 }
